f769a5d89d5f36392083fa73ddd3dda27080b2cc
Tong Thi Nhu Quynh <quynhfe99@gmail.com>
2026-08-07 03:42:13 +0700

    add project home ui - sidebar, project card, modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
   return (
     <html lang="vi" className={`${inter.variable} light`}>
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
